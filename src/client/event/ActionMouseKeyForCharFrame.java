6864992f63dfa00304caf37d1542b89267d1b1cd
Rembau <Rembau@Rembau-PC>
2013-04-12 11:20:55 +0800

    修改代码风格。

--- src/client/event/ActionMouseKeyForCharFrame.java
+++ src/client/event/ActionMouseKeyForCharFrame.java
@@ -17,165 +17,100 @@ import client.gui.ChatFrame;
 import client.gui.Login;
 import client.people.PeopleAttributeNow;
 
-public class ActionMouseKeyForCharFrame implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener, ActionListener 
-{
-   private ChatFrame cf;
-   private PeopleAttributeNow pa;
-   
-   /**
-    * @param cf
-    * @roseuid 50187B510053
-    */
-   public ActionMouseKeyForCharFrame(ChatFrame cf) 
-   {
-		this.cf=cf;
-		pa=cf.getPeople().getPeopleAttributeNow();    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B510055
-    */
-   public void keyPressed(KeyEvent e) 
-   {
-		if(e.getKeyCode()==KeyEvent.VK_ENTER && e.getModifiers()==2){
+public class ActionMouseKeyForCharFrame implements MouseListener,
+		MouseMotionListener, MouseWheelListener, KeyListener, ActionListener {
+	private ChatFrame cf;
+	private PeopleAttributeNow pa;
+
+	public ActionMouseKeyForCharFrame(ChatFrame cf) {
+		this.cf = cf;
+		pa = cf.getPeople().getPeopleAttributeNow();
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ENTER && e.getModifiers() == 2) {
 			pa.handle();
 			cf.getCommunication().sendCharMessage();
 			pa.init();
 		}
-		//System.out.println("keyPressed="+e.getKeyChar()+"=="+e.getKeyCode());    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B51005C
-    */
-   public void keyReleased(KeyEvent e) 
-   {
-		//System.out.println("keyReleased="+e.getKeyChar()+"=="+e.getKeyCode());    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B51005E
-    */
-   public void keyTyped(KeyEvent e) 
-   {
-		if((int)e.getKeyChar()==8){
+		// System.out.println("keyPressed="+e.getKeyChar()+"=="+e.getKeyCode());
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// System.out.println("keyReleased="+e.getKeyChar()+"=="+e.getKeyCode());
+	}
+
+	public void keyTyped(KeyEvent e) {
+		if ((int) e.getKeyChar() == 8) {
 			pa.addBackKeyUseNum();
-		} else{
-			//System.out.println(pa.getWordsNum());
-			if((int)e.getKeyChar()!=10){
-				if(pa.getWordsNum()==0){
+		} else {
+			// System.out.println(pa.getWordsNum());
+			if ((int) e.getKeyChar() != 10) {
+				if (pa.getWordsNum() == 0) {
 					pa.setStartTime();
 				}
 				pa.addWordsNum();
-				if(pa.getWordsNum()>=10){
+				if (pa.getWordsNum() >= 10) {
 					pa.handle();
 				}
 			}
 		}
-		//System.out.println("keyTyped="+e.getKeyChar()+"=="+e.getKeyCode()+"=="+(int)e.getKeyChar());    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B510060
-    */
-   public void mouseClicked(MouseEvent e) 
-   {
-		cf.getPeople().getPeopleAttributeNow().addMouseUseNum();    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B510067
-    */
-   public void mouseEntered(MouseEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B51006D
-    */
-   public void mouseExited(MouseEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B51006F
-    */
-   public void mousePressed(MouseEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B510071
-    */
-   public void mouseReleased(MouseEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B51007D
-    */
-   public void mouseDragged(MouseEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B51007F
-    */
-   public void mouseMoved(MouseEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B510081
-    */
-   public void mouseWheelMoved(MouseWheelEvent e) 
-   {
-    
-   }
-   
-   /**
-    * @param e
-    * @roseuid 50187B510092
-    */
-   public void actionPerformed(ActionEvent e) 
-   {
-		if(e.getActionCommand().trim().equals("发送")){
+		// System.out.println("keyTyped="+e.getKeyChar()+"=="+e.getKeyCode()+"=="+(int)e.getKeyChar());
+	}
+
+	public void mouseClicked(MouseEvent e) {
+		cf.getPeople().getPeopleAttributeNow().addMouseUseNum();
+	}
+
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	public void mouseExited(MouseEvent e) {
+
+	}
+
+	public void mousePressed(MouseEvent e) {
+
+	}
+
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	public void mouseDragged(MouseEvent e) {
+
+	}
+
+	public void mouseMoved(MouseEvent e) {
+
+	}
+
+	public void mouseWheelMoved(MouseWheelEvent e) {
+
+	}
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().trim().equals("发送")) {
 			pa.handle();
 			cf.getCommunication().sendCharMessage();
 			pa.init();
-		} else if(e.getActionCommand().trim().equals("开始训练")){
-			int select = JOptionPane.showConfirmDialog(cf, "在训练之前可以进行一些小测试，测试吗？");
-			if(select==JOptionPane.YES_OPTION){
-				
-			} else if(select== JOptionPane.NO_OPTION){
+		} else if (e.getActionCommand().trim().equals("开始训练")) {
+			int select = JOptionPane.showConfirmDialog(cf,
+					"在训练之前可以进行一些小测试，测试吗？");
+			if (select == JOptionPane.YES_OPTION) {
+
+			} else if (select == JOptionPane.NO_OPTION) {
 				cf.getCommunication().sendCharMessage("teacher");
-				((JButton)(e.getSource())).setText("结束训练");
+				((JButton) (e.getSource())).setText("结束训练");
 			}
-		} else if(e.getActionCommand().trim().equals("结束训练")){
+		} else if (e.getActionCommand().trim().equals("结束训练")) {
 			int select = JOptionPane.showConfirmDialog(cf, "你确定退出训练吗？");
-			if(select==JOptionPane.YES_OPTION){
+			if (select == JOptionPane.YES_OPTION) {
 				cf.getCommunication().sendCharMessage("end");
-				((JButton)(e.getSource())).setText("开始训练");
+				((JButton) (e.getSource())).setText("开始训练");
 			}
-		} else if(e.getActionCommand().trim().equals("登录")){
+		} else if (e.getActionCommand().trim().equals("登录")) {
 			new Login(cf);
-		}//end if    
-   }
+		}// end if
+	}
 }
