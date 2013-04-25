397e9faeabd2f5084b197135014361ac82798b45
Rembau <504229406@qq.com>
2013-04-25 18:19:41 +0800

    界面

---
+++ src/bot/client/gui/FontType.java
@@ -0,0 +1,9 @@
+package bot.client.gui;
+
+public class FontType {
+	private int size=12;
+	private String font_type;
+	private boolean italic;
+	private boolean bold=false;
+	private boolean dd;
+}
